c79500558118124c841fef281789134d1c929dde
kaho2580 <katsuma8888@gmail.com>
2022-08-03 14:36:29 +0900

    8-2まで

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,17 +2,22 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>復習</title>
+        <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>home</h1>
+        <h1>Blog Name</h1>
         <div class='posts'>
-            <div class='post'>
-                <h2 class='title'>Title</h2>
-                <p class='body'>This is a sample body.</p>
-            </div>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 
-Route::get('/posts','PostController@index');
+Route::get('/', 'PostController@index');
 
